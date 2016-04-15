cff677ac2e7c449cd4905c574d8a425581eeb8c9
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-04-15 14:23:18 +0200

    fix(js): fix dropdown menu positioning

--- views/default/elements/navigation/dropdown.js
+++ views/default/elements/navigation/dropdown.js
@@ -1,13 +1,16 @@
 require(['jquery', 'elgg/popup', 'jquery-ui'], function ($, popup) {
-
 	$(document).on('click', '.elgg-menu-item-has-dropdown > a', function (e) {
 		var $trigger = $(this);
 		var $target = $trigger.siblings('.elgg-child-menu').eq(0);
 		if ($trigger.length && $target.length) {
 			$target.addClass('elgg-menu-hover');
-			popup.open($trigger, $target);
+			var position = $trigger.data('position') || {};
+			position.at = position.at || $trigger.data('at');
+			position.my = position.my || $trigger.data('my');
+			position.collision = position.collision || $trigger('collision');
+			position.of = $target.parent();
+			popup.open($trigger, $target, position);
 			return false;
 		}
 	});
-
 });

98a7758d44031e19208e26e2d05dd35d387bf66c
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-11 08:09:18 +0200

    migrate(4.x): automated AST transformations
    - Generate elgg-plugin.php with view_extensions
    - Remove start.php and autoloader.php
    - Update manifest.xml to target Elgg 4.0

---
+++ elgg-plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'view_extensions' => [
+		'elements/navigation.css' => [
+			'elements/navigation/dropdown.css' => [],
+		],
+		'admin.css' => [
+			'elements/navigation/dropdown.css' => [],
+		],
+		'elgg.js' => [
+			'elements/navigation/dropdown.js' => [],
+		],
+	],
+
+];
